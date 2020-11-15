0f7a607f0166f20fce8ab105ec68e0eaac20a9b9
Ashley Wulber <ashtronaut@pm.me>
2020-11-15 18:23:36 -0500

    feat: add plane types

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
-use std::{convert::TryInto, ptr, mem};
-use std::f64;
+use std::{convert::TryInto, convert::TryFrom, ptr, mem};
 use rayon::{prelude::*};
 use half::f16;
+use noise;
+
 #[derive(Clone, Copy, Default)]
 #[repr(C)]
 struct Position32 {
@@ -77,21 +78,47 @@ pub struct Quad {
     v6: i32,
 }
 
+#[repr(C)]
+enum PlaneType
+{
+    Flat,
+    FBM,
+    Worley,
+}
+
+impl TryFrom<i32> for PlaneType {
+    type Error = ();
+
+    fn try_from(v: i32) -> Result<Self, Self::Error> {
+        match v {
+            x if x == PlaneType::Flat as i32 => Ok(PlaneType::Flat),
+            x if x == PlaneType::FBM as i32 => Ok(PlaneType::FBM),
+            x if x == PlaneType::Worley as i32 => Ok(PlaneType::Worley),
+            _ => Err(()),
+        }
+    }
+}
+
 // future goals: generate different wrapper functions for each type permutation and make get_plane generic...
 //  Vertex<f32, half::f16 | f32, u8 | f32>
 #[no_mangle]
-pub extern "C" fn fill_plane(vert_buf: ptr::NonNull<Vertex>, indx_buf: ptr::NonNull<Quad>, side_len: usize) -> *const u8 {
-    let v_count =  (side_len + 1) * (side_len + 1);
-    let tri_count = side_len * side_len;
-    let v_buffer: &mut [Vertex] = unsafe { std::slice::from_raw_parts_mut(vert_buf.as_ptr(), v_count) };
-    let indx_buffer: &mut [Quad] = unsafe { std::slice::from_raw_parts_mut(indx_buf.as_ptr(), tri_count) };
+pub extern "C" fn fill_plane(vert_buf: ptr::NonNull<Vertex>, indx_buf: ptr::NonNull<Quad>, side_len: usize, plane_type: i32) -> *const u8 {
+    match plane_type.try_into() {
+        Ok(plane_type) => {
+            let v_count =  (side_len + 1) * (side_len + 1);
+            let tri_count = side_len * side_len;
+            let v_buffer: &mut [Vertex] = unsafe { std::slice::from_raw_parts_mut(vert_buf.as_ptr(), v_count) };
+            let indx_buffer: &mut [Quad] = unsafe { std::slice::from_raw_parts_mut(indx_buf.as_ptr(), tri_count) };
 
-    fill_plane_buffers(v_buffer, indx_buffer, side_len);
+            fill_plane_buffers(v_buffer, indx_buffer, side_len, plane_type);
 
-    "OK\0".as_ptr()
+            "OK\0".as_ptr()
+        },
+        Err(_) => "Invalid plane type requested.\0".as_ptr()
+    }
 }
 
-fn fill_plane_buffers(v_buffer: &mut [Vertex], indx_buffer: &mut [Quad], side_len: usize) {
+fn fill_plane_buffers(v_buffer: &mut [Vertex], indx_buffer: &mut [Quad], side_len: usize, plane_type: PlaneType) {
     let half_side_len = side_len as f32 / 2f32;
     let vert_side: i32 = (side_len + 1).try_into().unwrap();
 
@@ -172,7 +199,7 @@ pub extern "C" fn get_plane(vptr: *mut *const Vertex, iptr: *mut *const Quad, si
     let mut v_buffer: Vec<Vertex> = vec![Default::default(); v_count];
     let mut indx_buffer: Vec<Quad> = vec![Default::default(); quad_count];
 
-    fill_plane_buffers(v_buffer.as_mut_slice(), indx_buffer.as_mut_slice(), side_len);
+    fill_plane_buffers(v_buffer.as_mut_slice(), indx_buffer.as_mut_slice(), side_len, PlaneType::Flat);
 
     unsafe {
         *vptr = v_buffer.as_ptr();
@@ -193,7 +220,7 @@ fn test_square_plane_one() {
     let mut v_buffer: Vec<Vertex> = vec![Default::default(); v_count];
     let mut indx_buffer: Vec<Quad> = vec![Default::default(); quad_count];
 
-    fill_plane_buffers(v_buffer.as_mut_slice(), indx_buffer.as_mut_slice(), side_len);   
+    fill_plane_buffers(v_buffer.as_mut_slice(), indx_buffer.as_mut_slice(), side_len, PlaneType::Flat);   
 }
 
 #[test]
@@ -205,5 +232,5 @@ fn test_square_plane_two() {
     let mut v_buffer: Vec<Vertex> = vec![Default::default(); v_count];
     let mut indx_buffer: Vec<Quad> = vec![Default::default(); quad_count];
 
-    fill_plane_buffers(v_buffer.as_mut_slice(), indx_buffer.as_mut_slice(), side_len);   
+    fill_plane_buffers(v_buffer.as_mut_slice(), indx_buffer.as_mut_slice(), side_len, PlaneType::Flat);   
 }
