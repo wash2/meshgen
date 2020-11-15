a61f379fbc40bfe83e79c230fc707554e797b006
Ashley Wulber <ashtronaut@pm.me>
2020-11-15 17:28:55 -0500

    feat: transfer plane data from rust to unity!

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,15 @@
-use std::ptr;
+use std::{convert::TryInto, ptr, mem};
 use std::f64;
-use rayon::prelude::*;
+use rayon::{prelude::*};
 use half::f16;
-
+#[derive(Clone, Copy, Default)]
 #[repr(C)]
 struct Position32 {
     x: f32,
     y: f32,
     z: f32,
 }
-
+#[derive(Clone, Copy, Default)]
 #[repr(C)]
 struct Normal16 {
     x: f16,
@@ -17,13 +17,14 @@ struct Normal16 {
     z: f16,
 }
 
+#[derive(Clone, Copy, Default)]
 #[repr(C)]
 struct Normal32 {
     x: f32,
     y: f32,
     z: f32,
 }
-
+#[derive(Clone, Copy, Default)]
 #[repr(C)]
 struct Tangent32 {
     w: f32,
@@ -32,6 +33,7 @@ struct Tangent32 {
     z: f32,
 }
 
+#[derive(Clone, Copy, Default)]
 #[repr(C)]
 struct TangentU8 {
     w: u8,
@@ -40,50 +42,65 @@ struct TangentU8 {
     z: u8,
 }
 
+#[derive(Clone, Copy, Default)]
+#[repr(C)]
+struct TexCoord32 {
+    u: f32,
+    v: f32
+}
+
+#[derive(Clone, Copy, Default)]
 #[repr(C)]
 pub struct Vertex {
     pos: Position32,
     norm: Normal32,
     tangent: Tangent32,
+    uv: TexCoord32,
 }
 
-#[no_mangle]
-pub extern "C" fn get_array(length: usize) -> *mut i32 {
-    let mut test = vec![68; length];
-    let ptr = test.as_mut_ptr();
-
-    std::mem::forget(test); // so that it is not destructed at the end of the scope
-
-    ptr
+#[derive(Clone, Copy, Default)]
+#[repr(C)]
+pub struct Triangle {
+    v1: i32,
+    v2: i32,
+    v3: i32
 }
 
-#[no_mangle]
-pub extern "C" fn hello_world() -> *const u8 {
-    "Hello, world!\0".as_ptr()
-}
-
-#[no_mangle]
-pub extern "C" fn fill_array(buf: ptr::NonNull<i32>, len: usize) -> usize {
-    let v_buffer: &mut [i32] = unsafe { std::slice::from_raw_parts_mut(buf.as_ptr(), len) };
-    v_buffer.par_iter_mut().enumerate().for_each(|(i, elem)| *elem = i as i32);
-    len
+#[derive(Clone, Copy, Default)]
+#[repr(C)]
+pub struct Quad {
+    v1: i32,
+    v2: i32,
+    v3: i32,
+    v4: i32,
+    v5: i32,
+    v6: i32,
 }
 
 // future goals: generate different wrapper functions for each type permutation and make get_plane generic...
 //  Vertex<f32, half::f16 | f32, u8 | f32>
 #[no_mangle]
-pub extern "C" fn get_plane(buf: ptr::NonNull<Vertex>, len: usize) -> usize {
-    let side_len =  (len as f64).sqrt() as usize;
-    let v_count =  side_len * side_len;
-    let v_buffer: &mut [Vertex] = unsafe { std::slice::from_raw_parts_mut(buf.as_ptr(), v_count) };
+pub extern "C" fn fill_plane(vert_buf: ptr::NonNull<Vertex>, indx_buf: ptr::NonNull<Quad>, side_len: usize) -> *const u8 {
+    let v_count =  (side_len + 1) * (side_len + 1);
+    let tri_count = side_len * side_len;
+    let v_buffer: &mut [Vertex] = unsafe { std::slice::from_raw_parts_mut(vert_buf.as_ptr(), v_count) };
+    let indx_buffer: &mut [Quad] = unsafe { std::slice::from_raw_parts_mut(indx_buf.as_ptr(), tri_count) };
 
+    fill_plane_buffers(v_buffer, indx_buffer, side_len);
+
+    "OK\0".as_ptr()
+}
+
+fn fill_plane_buffers(v_buffer: &mut [Vertex], indx_buffer: &mut [Quad], side_len: usize) {
     let half_side_len = side_len as f32 / 2f32;
+    let vert_side: i32 = (side_len + 1).try_into().unwrap();
 
     v_buffer.par_iter_mut().enumerate().for_each(|(i, cur_v)| {
+        
         cur_v.pos = Position32{
-            x: (-half_side_len + (i % side_len) as f32),
+            x: (-half_side_len + (i as i32 % vert_side) as f32),
             y: (0f32),
-            z: (-half_side_len + (i / side_len) as f32)
+            z: (-half_side_len + (i as i32 / vert_side) as f32)
         };
         cur_v.norm = Normal32{
             x: (0f32), 
@@ -91,12 +108,102 @@ pub extern "C" fn get_plane(buf: ptr::NonNull<Vertex>, len: usize) -> usize {
             z: (0f32)
         };
         cur_v.tangent = Tangent32{
-            w: (0f32),
-            x: (0f32), 
+            w: (1f32),
+            x: (1f32), 
             y: (0f32), 
             z: (0f32)
-        }
+        };
+        cur_v.uv = TexCoord32{
+            u: ((i as i32 % vert_side) as f32 / vert_side as f32),
+            v: ((i as i32 / vert_side) as f32 / vert_side as f32), 
+        };
     });
 
-    v_count
+    indx_buffer.par_iter_mut().enumerate().for_each(|(i, cur_tri)| {
+        let z = (i / side_len) as i32;
+        let x = (i % side_len) as i32;
+        let s = x * vert_side + z;
+        cur_tri.v1 = s + vert_side;
+        cur_tri.v2 = s + 1;
+        cur_tri.v3 = s;
+        cur_tri.v4 = s + 1;
+        cur_tri.v5 = s + vert_side;
+        cur_tri.v6 = s + vert_side + 1;
+    });
+}
+
+#[no_mangle]
+pub extern "C" fn get_plane_desc(side_len: usize, v_count: ptr::NonNull<i32>, e_count: ptr::NonNull<i32>, f_count: ptr::NonNull<i32>) -> *const u8 {
+    let _v_count = ((side_len + 1) * (side_len + 1)) as u64;
+    let _e_count = (2 * side_len + 3 * side_len * side_len) as u64;
+    let _f_count = (2 * side_len * side_len) as u64;
+
+    let _v_buf_bytes = _v_count * mem::size_of::<Vertex>() as u64;
+    let _e_buf_bytes = _e_count * mem::size_of::<i32>() as u64;
+    let _f_buf_bytes = _f_count * mem::size_of::<Triangle>() as u64;
+    
+    if _e_buf_bytes >= i32::MAX as u64 {
+        format!("Edge list would require too many bytes! {}\0",  _e_buf_bytes).as_ptr()
+    }
+    else if _v_buf_bytes >= i32::MAX as u64 {
+        format!("Vertex list would require too many bytes! {}\0",  _v_buf_bytes).as_ptr()
+    }
+    else if _f_buf_bytes >= i32::MAX as u64 {
+        format!("Face list would require too many bytes! {}\0",  _f_buf_bytes).as_ptr()
+    }
+    else {
+        unsafe {
+            let v_count_mut = v_count.as_ptr();
+            *v_count_mut = _v_count.try_into().unwrap();
+            let e_count_mut = e_count.as_ptr();
+            *e_count_mut = _e_count.try_into().unwrap();
+            let f_count_mut = f_count.as_ptr();
+            *f_count_mut = _f_count.try_into().unwrap();
+        }
+        "OK\0".as_ptr()
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn get_plane(vptr: *mut *const Vertex, iptr: *mut *const Quad, side_len: usize) -> usize {
+    let v_count =  (side_len + 1) * (side_len + 1);
+    let quad_count = side_len * side_len;
+
+    let mut v_buffer: Vec<Vertex> = vec![Default::default(); v_count];
+    let mut indx_buffer: Vec<Quad> = vec![Default::default(); quad_count];
+
+    fill_plane_buffers(v_buffer.as_mut_slice(), indx_buffer.as_mut_slice(), side_len);
+
+    unsafe {
+        *vptr = v_buffer.as_ptr();
+        *iptr = indx_buffer.as_ptr();
+    }
+    std::mem::forget(v_buffer); // so that it is not destructed at the end of the scope
+    std::mem::forget(indx_buffer); // so that it is not destructed at the end of the scope
+
+    0
+}
+
+#[test]
+fn test_square_plane_one() {
+    let side_len = 2;
+    let v_count =  (side_len + 1) * (side_len + 1);
+    let quad_count = side_len * side_len;
+
+    let mut v_buffer: Vec<Vertex> = vec![Default::default(); v_count];
+    let mut indx_buffer: Vec<Quad> = vec![Default::default(); quad_count];
+
+    fill_plane_buffers(v_buffer.as_mut_slice(), indx_buffer.as_mut_slice(), side_len);   
+}
+
+#[test]
+fn test_square_plane_two() {
+    let side_len = 2;
+    let v_count =  (side_len + 1) * (side_len + 1);
+    let quad_count = side_len * side_len;
+
+    let mut v_buffer: Vec<Vertex> = vec![Default::default(); v_count];
+    let mut indx_buffer: Vec<Quad> = vec![Default::default(); quad_count];
+
+    fill_plane_buffers(v_buffer.as_mut_slice(), indx_buffer.as_mut_slice(), side_len);   
 }
---
+++ unity/Plane.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Rendering;
+using UnityEngine;
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.Collections;
+using System.Runtime.InteropServices;
+using System;
+
+public class Plane : MonoBehaviour
+{
+    [DllImport("meshgen")]
+    private static extern IntPtr fill_plane(IntPtr vbuf, IntPtr ibuf, UIntPtr sideLength);
+    [DllImport("meshgen")]
+    private static extern IntPtr get_plane_desc(UIntPtr sideLength, out int vCnt, out int eCnt, out int fCnt);
+    
+    [System.Runtime.InteropServices.StructLayout(System.Runtime.InteropServices.LayoutKind.Sequential)]
+    struct ExampleVertex
+    {
+        public Vector3 pos;
+        public Vector3 normal;
+        public Vector4 tangent;
+        public Vector2 uv;
+    }
+
+    Vector3[] newVertices;
+    Vector2[] newUV;
+    int[] newTriangles;
+
+    public UIntPtr sideLength = (UIntPtr)10000;
+    public float frequency;
+    public Material m_Material;
+
+    void Start()
+    {
+        var t0 = Time.realtimeSinceStartup;
+
+        var layout = new[]
+        {
+            new VertexAttributeDescriptor(VertexAttribute.Position, VertexAttributeFormat.Float32, 3),
+            new VertexAttributeDescriptor(VertexAttribute.Normal, VertexAttributeFormat.Float32, 3),
+            new VertexAttributeDescriptor(VertexAttribute.Tangent, VertexAttributeFormat.Float32, 4),
+            new VertexAttributeDescriptor(VertexAttribute.TexCoord0, VertexAttributeFormat.Float32, 2),
+        };
+
+        gameObject.AddComponent<MeshFilter>();
+        gameObject.AddComponent<MeshRenderer>();
+        Mesh mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        GetComponent<MeshRenderer>().material = m_Material;
+        mesh.Clear();
+        
+        // fill Unity allocated NativeArray
+        unsafe {
+            int vertexCount;
+            int edgeCount;
+            int faceCount;
+            var res = Marshal.PtrToStringAnsi(
+                get_plane_desc(sideLength, out vertexCount, out edgeCount, out faceCount)
+            );
+            if (!res.Equals("OK")) {
+                Debug.Log(res);
+                return;
+            }
+
+            var verts = new NativeArray<ExampleVertex>(vertexCount, Allocator.Temp);
+            var tris = new NativeArray<int>(faceCount * 3, Allocator.Temp);
+            res = Marshal.PtrToStringAnsi(
+                fill_plane(new IntPtr(NativeArrayUnsafeUtility.GetUnsafePtr(verts)), new IntPtr(NativeArrayUnsafeUtility.GetUnsafePtr(tris)), sideLength)
+            );
+            if (!res.Equals("OK")) {
+                Debug.Log(res);
+                return;
+            }
+
+            mesh.SetVertexBufferParams(vertexCount, layout);
+            mesh.SetIndexBufferParams(faceCount * 3, UnityEngine.Rendering.IndexFormat.UInt32);
+
+            mesh.SetVertexBufferData(verts, 0, 0, vertexCount);
+            mesh.SetIndexBufferData(tris, 0, 0, faceCount * 3);
+
+            mesh.subMeshCount = 1;
+
+            var meshDesc = new UnityEngine.Rendering.SubMeshDescriptor(0, faceCount * 3, MeshTopology.Triangles);
+            mesh.SetSubMesh(0, meshDesc);
+        }
+
+        // Marshal rust created buffers - Seems unlikely to work well....
+        // figure out how to marshal without looping & PtrToStructure 
+        // https://social.msdn.microsoft.com/Forums/vstudio/en-US/7bd09baf-f78e-462f-9d65-7c3324350493/marshal-struct-containing-pointertoarrayofstructs-from-c-to-c?forum=clr
+
+                // int vertSide = sideLength + 1;
+        // newVertices = new Vector3[vertSide * vertSide];
+        // newUV = new Vector2[vertSide * vertSide];
+        // for (int x = 0; x < vertSide; x++) {
+        //     for (int z = 0; z < vertSide; z++) {
+        //         int cur = x * vertSide + z;
+        //         float v_x = -sideLength / 2f + x;
+        //         float v_z =  -sideLength / 2f + z;
+        //         newVertices[cur] = new Vector3(v_x, sideLength / 10 * Mathf.Cos(2 * Mathf.PI * frequency * (( Mathf.Abs(x- sideLength / 2f) + Mathf.Abs(z - sideLength / 2f)) / sideLength)), v_z);
+        //         // Debug.Log(sideLength / 2 * Mathf.Cos(2 * Mathf.PI * (x + z - sideLength)));
+        //     }
+        // }
+
+        // for (int x = 0; x < vertSide; x++) {
+        //     for (int z = 0; z < vertSide; z++) {
+        //         int cur = x * vertSide + z;
+        //         newUV[cur] = new Vector2((float)x / vertSide, (float)z / vertSide);
+        //     }
+        // }
+
+        // newTriangles = new int[sideLength * sideLength * 6];
+
+        // for (int x = 0; x < sideLength; x++) {
+        //     for (int z = 0; z < sideLength; z++) {
+        //         int s = x * vertSide + z;
+        //         int i = (x * sideLength + z) * 6;
+        //         newTriangles[i] = s;
+        //         newTriangles[i + 1] = s + 1;
+        //         newTriangles[i + 2] = s + vertSide;
+        //         newTriangles[i + 3] = s + vertSide + 1;
+        //         newTriangles[i + 4] = s + vertSide;
+        //         newTriangles[i + 5] = s + 1;
+        //     }
+        // }
+
+
+        // gameObject.AddComponent<MeshFilter>();
+        // gameObject.AddComponent<MeshRenderer>();
+        // Mesh mesh = new Mesh();
+        // GetComponent<MeshFilter>().mesh = mesh;
+        // GetComponent<MeshRenderer>().material = m_Material;
+        // mesh.Clear();
+
+
+        // mesh.SetIndexBufferParams(newVertices.Length, UnityEngine.Rendering.IndexFormat.UInt32);
+        // mesh.vertices = newVertices;
+        // mesh.uv = newUV;
+        // mesh.triangles = newTriangles;
+    }
+}
